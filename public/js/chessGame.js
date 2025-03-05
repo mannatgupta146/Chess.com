607d52946c13aafbc62b8d0734308ea79943a625
mannatgupta146 <mannatgupta146@gmail.com>
2025-03-06 00:51:58 +0530

    added

--- public/js/chessGame.js
+++ public/js/chessGame.js
@@ -1,2 +1,47 @@
 const socket = io();
 
+const chess = new Chess();
+const boardElement = document.querySelector('.chessboard');  
+
+let dragPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row,rowIndex)=>{
+        row.forEach((square,squareIndex)=>{
+            const squareElement = document.createElement('div');
+            squareElement.classList.add('square',
+                (rowIndex+squareIndex) % 2 === 0?'light':'dark');
+
+        squareElement.dataset.row = rowIndex;
+        squareElement.dataset.square = squareIndex;
+        
+        if(square){
+            const pieceElement = document.createElement('div');
+            pieceElement.classList.add('piece',
+                square.color === "w" ? "white" : "black",
+            );
+            pieceElement.innerText = "";
+            pieceElement.draggable = playerRole === square.color;
+
+            pieceElement.addEventListener('dragstart',()=>{
+                if(pieceElement.draggable){
+                    dragPiece = pieceElement;
+                    sourceSquare = {row:rowIndex, col:squareIndex};
+                }
+            });
+            
+        }
+
+        })
+    })
+}
+
+const handleMove = () => {}
+
+const getPieceUnicode = () => {}
+
+renderBoard();
